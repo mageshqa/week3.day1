2be46af6f950d8562c43456b26b0399a32ed0eba
mageshqa <100154769+mageshqa@users.noreply.github.com>
2022-03-05 16:07:53 +0530

    Add files via upload

---
+++ Assignment1/Simple Inheritance Main/Desktop.java
@@ -0,0 +1,19 @@
+package org.system;
+
+public class Desktop extends Computer{
+
+	public static void desktopSize()
+	{
+		System.out.println("From Desktop Size");
+		
+	}
+	
+	
+	public static void main(String [] args)
+	{
+		Computer.computerModel();
+		Desktop.desktopSize();
+				
+	}
+
+}
